9d74ec9f83b714aa97708ce78aa277100ab3fcd1
mrinmoy <mrinmoy.low2002@gmail.com>
2025-04-02 12:36:43 +0530

    Final debug True

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def predict():
     return render_template('home.html', prediction_text=f'Predicted Price: ${output:,.2f}')
 
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', port=8080) 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True) 
    
      
